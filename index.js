71a77e93464cc5182673cf0f11c48d942cf45be5
stefanovae743-lang <stefanovae743@gmail.com>
2026-09-19 22:09:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+const http = require('http');
+
+http.createServer((req, res) => {
+  res.write("Bot is alive!");
+  res.end();
+}).listen(8080, () => {
+  console.log("Keep-alive server is running on port 8080");
+});
 import './src/app.js';
