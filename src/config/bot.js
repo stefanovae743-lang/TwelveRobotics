c1c690e315368031abe35368bb91993ed1d42941
stefanovae743-lang <stefanovae743@gmail.com>
2026-09-20 12:53:33 +0100

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -141,7 +141,7 @@ export const botConfig = {
     },
     footer: {
       // Default footer text used in bot embeds.
-      text: "Titan Bot",
+      text: "Ran by @twelvebraincells, any queries DM user via /dm",
       // Footer icon URL (null = no icon).
       icon: null,
     },
