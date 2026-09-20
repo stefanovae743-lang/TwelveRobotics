d6bf2d10000337b81c1ba7043e23ddd668d1424e
stefanovae743-lang <stefanovae743@gmail.com>
2026-09-20 12:28:31 +0100

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -93,8 +93,8 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#336699",
-      secondary: "#2F3136",
+      primary: "#424000",
+      secondary: "#400000",
 
       // Standard status colors for success/error/warning/info messages.
       success: "#57F287",
